a2fd405e2606a0721baae9052e9a04eb6b63395f
Ashikur38 <mdashikurrahman18@gmail.com>
2023-06-21 18:26:34 +0600

    build file added

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    // Configuration options go here
+    // For example:
+    build: {
+        outDir: 'dist',
+        minify: true,
+        target: 'esnext'
+    },
+});
